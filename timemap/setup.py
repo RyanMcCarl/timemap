286418bf4df4cc7b824cf07a42cb2157c240c0d4
Ryan McCarl <ryan.mccarl@gmail.com>
2017-08-01 12:31:02 -0600

    First commit

---
+++ timemap/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'TimeMap',
+    packages = ['TimeMap'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Library to handle and sync task data between different systems',
+    author = 'Ryan McCarl',
+    author_email = 'admin@wordbrewery.com',
+    url = 'https://github.com/RyanMcCarl/timemap', # use the URL to the github repo
+    keywords = ['org-mode', 'todotxt', 'todoapp'],
+    classifiers = [],
+)
